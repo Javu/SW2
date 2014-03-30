f9d4f495b94626b6bb707c2cfe6d500c021cebfe
jsem <james.robert.semple@gmail.com>
2014-03-31 00:01:00 +1100

    added server test file

---
+++ src/test/java/fantasyteam/sw2/ServerTest.java
@@ -0,0 +1,15 @@
+package fantasyteam.sw2;
+
+import org.testng.annotations.Test;
+
+/**
+ * Tests for base server class
+ * @author jamessemple
+ */
+public class ServerTest {
+    
+    @Test
+    public void testServer() {
+        
+    }
+}
