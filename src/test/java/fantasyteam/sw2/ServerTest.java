82361705d5be51abae852ba2a8327b3eb6d6c21b
jsem <james.robert.semple@gmail.com>
2014-03-31 00:22:14 +1100

    added beforetest method for ServerTest

--- src/test/java/fantasyteam/sw2/ServerTest.java
+++ src/test/java/fantasyteam/sw2/ServerTest.java
@@ -1,5 +1,6 @@
 package fantasyteam.sw2;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -7,6 +8,16 @@ import org.testng.annotations.Test;
  * @author jamessemple
  */
 public class ServerTest {
+    
+    Server server1;
+    Server server2;
+    
+    @BeforeMethod
+    private void setupServer() {
+        server1 = new Server();
+        server2 = new Server();
+    }
+    
     
     @Test
     public void testServer() {
