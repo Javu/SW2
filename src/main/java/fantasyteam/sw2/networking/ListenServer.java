8e17c45a0fe137b48a31bc26df4c0eb66b31d97a
Javu <javuthescrunge@hotmail.com>
2014-04-01 01:34:31 +1100

    Implemented ListenServer and ListenThread correctly

--- src/main/java/fantasyteam/sw2/networking/ListenServer.java
+++ src/main/java/fantasyteam/sw2/networking/ListenServer.java
@@ -3,28 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package fantasyteam.sw2.networking;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
  * @author javu
  */
-public class ListenServer extends Server{
-    ServerSocket server_socket;
-    
-    public ListenServer(){
+public class ListenServer extends Server {
+
+    protected ListenThread listen_thread;
+
+    public ListenServer() throws IOException {
         super();
-        server_socket = null;
+        listen_thread = new ListenThread(this);
     }
-    
-    public ListenServer(int p){
+
+    public ListenServer(int p) throws IOException {
         super(p);
-        server_socket = null;
+        listen_thread = new ListenThread(this);
     }
     
+    /**
+     * Creates a ServerSocket and puts {@link Server} into listen mode.
+     * @return The hash assigned to the SocketThread.
+     * @throws IOException
+     */
+    public String listen() throws IOException {
+        Socket temp_socket = listen_thread.getServerSocket().accept();
+        Sock temp_sock = new Sock(temp_socket);
+        String hash = generateUniqueHash();
+        socket_list.put(hash, new SocketThread(temp_sock, this, hash));
+        socket_list.get(hash).start();
+        return hash;
+    }
     
-    
+    public void startThread(){
+        listen_thread.start();
+    }
+
 }
--- src/main/java/fantasyteam/sw2/networking/Server.java
+++ src/main/java/fantasyteam/sw2/networking/Server.java
@@ -197,6 +197,10 @@ public class Server {
         socket_list.remove(old_hash);
     }
 
+    public void startThread(){
+        
+    }
+    
     public String listen() throws IOException {
         return "";
     }
